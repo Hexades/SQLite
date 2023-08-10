ad97aa386aff65e5939dc1d1584c823854596efa
Ranx0r0x <register@bradleejohnson.com>
2023-08-10 01:12:20 -0500

    Fixing recursive issue with functions in structs

--- events.go
+++ events.go
@@ -4,76 +4,77 @@ import (
 	bus "github.com/hexades/hexabus"
 )
 
+// TODO The recursion issue requiring the store of function pointer and extra referencing is unappealing.
 type SQLiteEvent interface {
 	Execute(repo *repository)
 }
 
 func NewOpen(connection string, openFunc OpenFunction) *Open {
-	return &Open{connection: connection, openFunc: openFunc}
+	return &Open{connection: connection, openFunc: &openFunc}
 }
 
 type Open struct {
 	bus.Event
 	connection string
-	openFunc   OpenFunction
+	openFunc   *OpenFunction
 }
 
 func (e *Open) Execute(repo *repository) {
-	e.openFunc(e, repo)
+	(*e.openFunc)(e, repo)
 }
 
 func NewRead(queryValue any, readFunc ReadFunction) *Read {
-	return &Read{queryValue: queryValue, readFunc: readFunc}
+	return &Read{queryValue: queryValue, readFunc: &readFunc}
 }
 
 type Read struct {
 	bus.Event
-	readFunc   ReadFunction
+	readFunc   *ReadFunction
 	queryValue any
 }
 
 func (e *Read) Execute(repo *repository) {
-	e.readFunc(e, repo)
+	(*e.readFunc)(e, repo)
 }
 
 func NewInsert(value any, insertFunc InsertFunction) *Insert {
-	return &Insert{value: value, insertFunc: insertFunc}
+	return &Insert{value: value, insertFunc: &insertFunc}
 }
 
 type Insert struct {
 	bus.Event
 	value      any
-	insertFunc InsertFunction
+	insertFunc *InsertFunction
 }
 
 func (e *Insert) Execute(repo *repository) {
-	e.insertFunc(e, repo)
+	(*e.insertFunc)(e, repo)
 }
 
 func NewUpdate(value any, updateFunc UpdateFunction) *Update {
-	return &Update{value: value, updateFunc: updateFunc}
+	return &Update{value: value, updateFunc: &updateFunc}
 }
 
 type Update struct {
 	bus.Event
-	value any
-	updateFunc UpdateFunction
-}
-func (e *Update) Execute(repo *repository) {
-	e.updateFunc(e, repo)
+	value      any
+	updateFunc *UpdateFunction
 }
 
+func (e *Update) Execute(repo *repository) {
+	(*e.updateFunc)(e, repo)
+}
 
 func NewDelete(value any, deleteFunc DeleteFunction) *Delete {
-	return &Delete{value: value, deleteFunc: deleteFunc}
+	return &Delete{value: value, deleteFunc: &deleteFunc}
 }
 
 type Delete struct {
 	bus.Event
-	value any
-	deleteFunc DeleteFunction
+	value      any
+	deleteFunc *DeleteFunction
 }
 
 func (e *Delete) Execute(repo *repository) {
-	e.deleteFunc(e, repo)
+	(*e.deleteFunc)(e, repo)
 }
